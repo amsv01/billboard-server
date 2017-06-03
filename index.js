a79b555b98239a63f06f1d4e396dd45f01d8f421
Amin Mousavi <tigmin@gmail.com>
2017-06-03 22:20:30 +0430

    - Add server files. - Add yarn lock

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const
+  devMode = (process.env.NODE_ENV !== 'production'),
+  express = require('express'),
+  graphqlHTTP = require('express-graphql'),
+  app = express(),
+  compression = require('compression'),
+  {buildSchema} = require('graphql');
+
+const shouldCompress = (req, res) => {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+};
+
+app.use(compression({
+  filter: shouldCompress
+}));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+  hello: () => 'Hello world!'
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(app.get('port'));
