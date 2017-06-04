97534cd7067e023663169414d0e58628c6676a35
Amin Mousavi <tigmin@gmail.com>
2017-06-04 11:53:20 +0430

    Integrate knex

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/billboard_test',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/billboard_dev',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+};
